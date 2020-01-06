65ddcf050c2efd0f54cb4ba87117e660f3d7681b
Micael Dias <diasmicaelandre@gmail.com>
2020-01-06 21:09:40 +0000

    Move headers to include/ folder

---
+++ include/libroomba.h
@@ -0,0 +1,32 @@
+#ifndef __COMMUM_H__
+#define __COMMUM_H__
+
+// the usual suspects
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+// socket includes
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+// Json
+#include <json.h>
+
+// wolfSSL
+#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
+
+#define TLS_PORT        8883
+#define DISCOVERY_PORT  5678
+#define BUFSIZE         1024
+
+void error(const char *msg);
+void discovery();
+void getRobotInfo(const char* host);
+int getPassword(const char* host);
+
+#endif
