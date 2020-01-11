0095af18cfcd984012d171842e25d50a5c17a0a2
Micael Dias <diasmicaelandre@gmail.com>
2020-01-11 18:34:32 +0000

    Improve example

--- examples/startRoomba.c
+++ examples/startRoomba.c
@@ -4,12 +4,12 @@ int main(int argc, char const *argv[]) {
 
   int rc;
 
-  rc = initRoomba("192.168.1.94", "blid", "psw");
+  rc = initRoomba("192.168.1.94", "3178480822035600", ":1:1553461121:VD9uFW2Omfq336Vu");
   if (rc == MQTT_CODE_SUCCESS) {
     printf("Connected\n");
   }
 
-  rc = sendCommand("cmd", "start");
+  rc = sendCommand("cmd", argv[1]);
   if (rc == MQTT_CODE_SUCCESS) {
     printf("Sent\n");
   }
